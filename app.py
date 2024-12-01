72d38336fc38e71b5ee76ac5c8c323a3cd491c2c
Anwar Andargie <anwarandargie3360@gmail.com>
2024-12-01 07:08:45 +0300

    add route to the homepage

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
 if __name__ == 'main':
   app.run()
+
+@app.route('/')
+def index():
+  return render_template('home.html')
