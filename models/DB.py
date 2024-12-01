bd94ebffef45947086260253ee6715d21c5f1243
Anwar Andargie <anwarandargie3360@gmail.com>
2024-12-01 10:41:44 +0300

    add mysql database connection

---
+++ models/DB.py
@@ -0,0 +1,19 @@
+from flaskext.mysql import MySQL
+class DB():
+	host = "localhost"
+	user = "root"
+	password = ""
+	db = "lms"
+	table = ""
+
+	def __init__(self, app):
+		app.config["MYSQL_DATABASE_HOST"] = self.host
+		app.config["MYSQL_DATABASE_USER"] = self.user
+		app.config["MYSQL_DATABASE_PASSWORD"] = self.password
+		app.config["MYSQL_DATABASE_DB"] = self.db
+
+		self.mysql = MySQL(app)
+	def get_db_cursor(self):
+		conn = self.mysql.connect()
+		return self.conn, self.conn.cursor()
+
